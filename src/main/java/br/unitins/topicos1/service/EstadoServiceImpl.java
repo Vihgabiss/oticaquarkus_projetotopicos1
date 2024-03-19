ff628cf5e4a2b876cac0a044a84934aff283b0ee
Vitória Gabriela <vitoriagcsantana@gmail.com>
2024-03-19 18:38:29 -0300

    alterando o insert de cidade

--- src/main/java/br/unitins/topicos1/service/EstadoServiceImpl.java
+++ src/main/java/br/unitins/topicos1/service/EstadoServiceImpl.java
@@ -1,6 +1,7 @@
 package br.unitins.topicos1.service;
 
 import java.util.List;
+import java.util.Set;
 
 import br.unitins.topicos1.dto.CidadeDTO;
 import br.unitins.topicos1.dto.CidadeResponseDTO;
@@ -13,7 +14,10 @@ import br.unitins.topicos1.repository.EstadoRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Valid;
+import jakarta.validation.Validator;
 import jakarta.ws.rs.NotFoundException;
 
 @ApplicationScoped
@@ -24,6 +28,9 @@ public class EstadoServiceImpl implements EstadoService{
 
     @Inject
     CidadeRepository cidadeRepository;
+
+    @Inject
+    Validator validator;
 
     @Override
     @Transactional
@@ -82,18 +89,33 @@ public class EstadoServiceImpl implements EstadoService{
                
     }
 
+    private void validar(CidadeDTO dto) throws ConstraintViolationException{
+        Set<ConstraintViolation<CidadeDTO>> violations = validator.validate(dto);
+        if(!violations.isEmpty())
+            throw new ConstraintViolationException(violations);
+    }
+
     @Override
     @Transactional
     public CidadeResponseDTO insertCidade(@Valid CidadeDTO dto) {
-        Estado estado = repository.findById(dto.idEstado());
+        // Estado estado = repository.findById(dto.idEstado());
 
-        Cidade cidade = new Cidade();
-        cidade.setNome(dto.nome());
-        cidade.setIdEstado(estado);
+        // Cidade cidade = new Cidade();
+        // cidade.setNome(dto.nome());
+        // cidade.setIdEstado(estado);
 
-        cidadeRepository.persist(cidade);
+        // cidadeRepository.persist(cidade);
 
-        return CidadeResponseDTO.valueOf(cidade);
+        // return CidadeResponseDTO.valueOf(cidade);
+
+        validar(dto);
+
+        Cidade entity = new Cidade();
+        entity.setNome(dto.nome());
+        entity.setIdEstado(repository.findById(dto.idEstado()));
+
+        cidadeRepository.persist(entity);
+        return CidadeResponseDTO.valueOf(entity);
     }
 
     @Override
