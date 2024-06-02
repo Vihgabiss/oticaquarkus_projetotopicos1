65ca719b11429e5d4ef6387edede3f99d2a40294
ana-bmm0623 <anamarques@unitins.br>
2024-06-02 16:36:19 -0300

    Fabricante - updates

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,6 +4,7 @@
 -- insert into myentity (id, field) values(2, 'field-2');
 -- insert into myentity (id, field) values(3, 'field-3');
 -- alter sequence myentity_seq restart with 4;
+ALTER TABLE fabricante_marca DROP CONSTRAINT fabricante_marca_id_marca_key;
 
 --Inserção de Marca
 insert into marca(nome) values('Ray-Ban');
