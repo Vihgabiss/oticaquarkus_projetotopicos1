cdbc7b9e133f6676c59295e21a45caddd3d1fab5
ana-bmm0623 <anamarques@unitins.br>
2024-06-06 14:06:17 -0300

    alterações

--- src/main/java/br/unitins/topicos1/dto/VendaDTO.java
+++ src/main/java/br/unitins/topicos1/dto/VendaDTO.java
@@ -2,15 +2,17 @@ package br.unitins.topicos1.dto;
 
 import java.util.List;
 
+import jakarta.validation.constraints.NotNull;
+
 public record VendaDTO(
-        // @NotNull(message = "O id do tipo de pagamento não pode ser nulo")
+        @NotNull(message = "O id do tipo de pagamento não pode ser nulo")
          Long idTipoPagamento,
 
         List<ItemVendaDTO> itens,
 
         String cupom,
 
-        // @NotNull(message = "O id do endereço não pode ser nulo") 
+        @NotNull(message = "O id do endereço não pode ser nulo") 
         Long idEnderecoEntrega) {
 
 }
